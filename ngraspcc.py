d84be067402111fc9806c8a6392baa5e8177d456
lalvarenga <luis.alvarenga@aluno.cefet-rj.br>
2022-07-28 16:55:34 -0300

    atualização

--- ngraspcc.py
+++ ngraspcc.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3.7
 
-# Copyright 2022, Gurobi Optimization, LLC
-
-# This example formulates and solves the following simple MIP model:
-#  maximize
-#        x +   y + 2 z
-#  subject to
-#        x + 2 y + 3 z <= 4
-#        x +   y       >= 1
-#        x, y, z binary
-
 import gurobipy as gp
 from gurobipy import GRB
 
 try:
     B = 2
-    T = 20
+    T = 5
     P = 3
     Vmax = 4
 
@@ -24,12 +14,35 @@ try:
     C[1] = 1.2
     C[2] = 0.8
 
-    D = {}
-    D[0] = 5
-    D[1] = 2
-    D[2] = 8
+    DS = {}
+    DS[0] = 3
+    DS[1] = 6
+    DS[2] = 4
+
+    ds = {}
+    ds[0] = 14
+    ds[1] = 10
     
-    Cmax = 5
+    MC = {}
+    MC[0] = 2
+    MC[1] = 8
+    MC[2] = 6
+
+    mc = {}
+    mc[0] = 16
+    mc[1] = 24
+
+    GF = {}
+    GF[0] = 100
+    GF[1] = 300
+    GF[2] = 200
+
+    gf = {}
+    gf[0] = 1000
+    gf[1] = 1300
+
+
+    Cmax = 10
     Dmax = 20
     Tmax = 10
 
@@ -42,32 +55,76 @@ try:
         for i in range(Vmax):
             for t in range(T):
                 for b in range(B):
-                    x_pitb[p,i,t,b] = m.addVar(obj=1,  vtype=gp.GRB.BINARY, name='x_' + str(p) + '_' + str(i) + '_' + str(t) + '_' + str(b) )
+                    x_pitb[p,i,t,b] = m.addVar(obj=0,  vtype=gp.GRB.BINARY, name='x_' + str(p) + '_' + str(i) + '_' + str(t) + '_' + str(b) )
 
     # Set objective
-    objective = gp.quicksum(x_pitb[p,i,t,b] * C[p] for b in range( B) for t in range( T) for i in range( Vmax) for p in range( P))
+    objective = gp.quicksum(x_pitb[p,i,t,b] * C[p] for b in range( B) for t in range(T) for i in range( Vmax) for p in range( P))
 
-    m.ModelSense = gp.GRB.MAXIMIZE
+    m.ModelSense = gp.GRB.MINIMIZE
     m.setObjective(objective)
 
     # Add constraint: Cost
     m.addConstr(gp.quicksum(x_pitb[p,i,t,b] * C[p] for b in range(B) for t in range(T) for i in range(Vmax) for p in range(P)) <= Cmax, "Cost")
 
     # Add constraint: Time
-    for t in range(T):
-        m.addConstr(gp.quicksum(x_pitb[p,i,t,b] for b in range(B) for i in range(Vmax) for p in range(P)) <= Tmax, "Time")
+    m.addConstr(gp.quicksum(x_pitb[p,i,t,b] for b in range(B) for t in range(T) for i in range(Vmax) for p in range(P)) <= Tmax, "Time")
  
-    # Add constraint: Storage
-    for t in range(T):
-        for b in range(B):
-            m.addConstr(
-                gp.quicksum(
-                    x_pitb[p,i,t,b] * D[p] for i in range(Vmax) for p in range(P)
-                )
-                    <= 
-                gp.quicksum(
-                    Dmax * x_pitb[pp,ii,t,b] for ii in range(Vmax) for pp in range(P)
-                ), "Storage")
+#    # Add constraint: Storage
+#    for t in range(T):
+#        for b in range(B):
+#            m.addConstr(
+#                gp.quicksum(
+#                    x_pitb[p,i,t,b] * DS[p] for i in range(Vmax) for p in range(P)
+#                )
+#                    >= 
+#                gp.quicksum(
+#                   x_pitb[pp,ii,t,b] * ds[b] for ii in range(Vmax) for pp in range(P)
+#                ), "Storage")
+
+#    # Add constraint: Memory
+#    for t in range(T):
+#        for b in range(B):
+#            m.addConstr(
+#                gp.quicksum(
+#                    x_pitb[p,i,t,b] * MC[p] for i in range(Vmax) for p in range(P)
+#                )
+#                    >= 
+#                gp.quicksum(
+#                    x_pitb[pp,ii,t,b] * mc[b] for ii in range(Vmax) for pp in range(P)
+#                ), "Memory")
+
+    # Add constraint: GFlops
+    for b in range(B):
+        m.addConstr(
+            gp.quicksum(
+                x_pitb[p,i,t,b] * GF[p] for i in range(Vmax) for p in range(P) for t in range(T)
+            )
+            >= 
+            gf[b], "GFlops")
+
+    # Add constraint: Precursor
+    for p in range(P):
+        for i in range(Vmax-1):
+            for t in range(T):
+                for b in range(B):
+                    m.addConstr(
+                        x_pitb[p,i+1,t,b] 
+                        <= 
+                        x_pitb[p,i,t,b] 
+                        , "Precursor")
+
+    # Add constraint: Sucessor
+    for p in range(P):
+        for i in range(Vmax):
+            for t in range(T):
+                for b in range(B):
+                    m.addConstr(
+                        gp.quicksum(
+                            x_pitb[p,ii,t,bb] for ii in range(i) for bb in range(b, B)
+                        )
+                        <= 
+                        (1 - x_pitb[p,i,t,b]) 
+                        , "Sucessor")
 
     # Optimize model
     m.update()
@@ -77,7 +134,7 @@ try:
         print('%s %s' % (v.VarName, v.X))
 
     print('Obj: %g' % m.ObjVal)
-
+#
 except gp.GurobiError as e:
     print('Error code ' + str(e.errno) + ': ' + str(e))
 
