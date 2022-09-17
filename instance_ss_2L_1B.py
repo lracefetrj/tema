01aefc4aa1d79c9bee235eaf044e817fac8899da
lalvarenga <luis.alvarenga@aluno.cefet-rj.br>
2022-09-17 19:42:05 -0300

    testes no modelo

---
+++ instance_ss_2L_1B.py
@@ -0,0 +1,53 @@
+import ngraspcclb as model
+
+P = 1  # VM Type
+L = 2  # Layers
+
+GF = {} # GFLOPS {vm type}
+GF[0] = (100)
+GF[1] = (332.8    * 3600)
+GF[2] = (1123.856 * 3600)
+
+MC = {} # RAM {vm type}
+MC[0] = 10
+MC[1] = 60.5
+MC[2] = 23
+
+DS = {} # DISK {vm type}
+DS[0] = 10
+DS[1] = 3360 
+DS[2] = 1680
+
+C = {} # COST {vm type}
+C[0] = 1
+C[1] = 2.4
+C[2] = 2.1
+
+B = {} # Bucket {layer}
+B[0] = 1
+B[1] = 1
+
+ds = {} # DISK {layer, bucket}
+ds[0,0] = 20
+ds[1,0] = 20
+
+mc = {} # RAM {layer, bucket}
+mc[0,0] = 20
+mc[1,0] = 20
+
+gf = {} # GFLOPS {layer, bucket}
+gf[0,0] = 220
+gf[1,0] = 220
+
+v = {} # VM {layer, bucket}
+v[0,0] = 2
+v[1,0] = 2
+
+Cmax = 20 # Max Cost
+Tmax = 10 # Max Time
+Vmax = 2 # Max VM
+
+alpha1 = 0.5
+alpha2 = 0.5
+
+model.solucionar(P, GF, MC, DS, C, B, L, gf, mc, ds, v, Cmax, Vmax, (Tmax+1), alpha1, alpha2, __file__)
--- ngraspcclb.py
+++ ngraspcclb.py
@@ -30,132 +30,150 @@ def solucionar(P, GF, MC, DS, C, B, L, gf, mc, ds, v, Cmax, Vmax, Tmax, alpha1, 
 
         # Set objective
         objective = (alpha1 * gp.quicksum(
-                        (z_pi[p,i] * C[p] for p in range(P) for i in range( Vmax))
+                        (z_pi[p,i] * C[p] for p in range(P) for i in range(Vmax))
                     ) / Cmax) + (alpha2 * (z / Tmax))
 
         m.ModelSense = gp.GRB.MINIMIZE
         m.setObjective(objective)
 
         # Add constraint: C40
+        j = 0
         for pp in range(P):
             for ii in range(Vmax):
                 for t in range(1,Tmax):
                     for l in range(L):
                         for b in range(B[l]):
+                            j += 1
                             m.addConstr(
                                 gp.quicksum(
                                     DS[p] * x_pitbl[p,i,t,b,l] for p in range(P) for i in range(Vmax) 
                                 )
                                 >= 
                                 ds[l,b] * x_pitbl[pp,ii,t,b,l]
-                                , "C40")
+                                , "C40_" + str(j))
 
 
         # Add constraint: C41
+        j = 0
         for pp in range(P):
             for ii in range(Vmax):
                 for t in range(1,Tmax):
                     for l in range(L):
                         for b in range(B[l]):
+                            j += 1
                             m.addConstr(
                                 gp.quicksum(
                                     MC[p] * x_pitbl[p,i,t,b,l] for p in range(P) for i in range(Vmax) 
                                 )
                                 >= 
                                 mc[l,b] * x_pitbl[pp,ii,t,b,l]
-                                , "C41")
+                                , "C41_" + str(j))
 
 
         # Add constraint: C42
+        j = 0
         for l in range(L):
             for b in range(B[l]):
+                j += 1
                 m.addConstr(
                     gp.quicksum(
                         x_pitbl[p,i,t,b,l] * GF[p] for p in range(P) for i in range(Vmax) for t in range(1,Tmax)
                     )
                     >= 
                     gf[l,b]
-                    , "C42")
+                    , "C42_" + str(j))
 
         # Add constraint: C43
+        j = 0
         for p in range(P):
             for i in range(Vmax):
                 for t in range(1,Tmax):
                     for l in range(L):
                         for b in range(B[l]):
+                            j += 1
                             m.addConstr(
                                 gp.quicksum(
                                     x_pitbl[p,ii,t,bb,l] for ii in range(i) for bb in range((b+1), B[l])
                                 )
                                 <= 
                                 (1 - x_pitbl[p,i,t,b,l]) * M
-                                , "C43")                    
+                                , "C43_" + str(j))                    
 
         # Add constraint: C44
+        j = 0
         for p in range(P):
             for i in range(Vmax-1):
                 for t in range(1,Tmax):
+                        j += 1
                         m.addConstr(
                             gp.quicksum( x_pitbl[p,i+1,t,b,l] for l in range(L) for b in range(B[l]))
                             <= 
                             gp.quicksum( x_pitbl[p,i,t,b,l] for l in range(L) for b in range(B[l]))
-                            , "C44")
+                            , "C44_" + str(j))
 
 
         # Add constraint: C45
+        j = 0
         for p in range(P):
-            for i in range(Vmax):
-                for t in range(1,Tmax):
-                    for l in range((L-1)):
-                        for b in range(B[l]):
-                            m.addConstr(
-                                gp.quicksum(
-                                    x_pitbl[pp,ii,tt,bb,ll] for pp in range(P) for ii in range(Vmax) for tt in range(1,t) for ll in range(l+1, L) for bb in range(B[ll]) 
-                                )
-                                <= 
-                                (1 - x_pitbl[p,i,t,b,l]) * M
-                                , "C45")
-
-
+           for i in range(Vmax):
+               for t in range(1,Tmax):
+                   for l in range(L):
+                       for b in range(B[l]):
+                           j += 1
+                           m.addConstr(
+                               gp.quicksum(
+                                   x_pitbl[pp,ii,tt,bb,ll] for pp in range(P) for ii in range(Vmax) for tt in range(1,t+1) for ll in range(l+1, L) for bb in range(B[ll]) 
+                               )
+                               <= 
+                               (1 - x_pitbl[p,i,t,b,l]) * M
+                               , "C45_" + str(j))
 
         # Add constraint: C46
+        j = 0
         for t in range(1,Tmax):
+            j += 1
             m.addConstr(
                 gp.quicksum(
                    x_pitbl[p,i,t,b,l] for p in range(P) for i in range(Vmax) for l in range(L) for b in range(B[l])
                 )
                 <= 
-                Vmax, "C46")
+                Vmax, "C46_" + str(j))
 
         # Add constraint: C47
+        j = 0
         for t in range(1,Tmax):
             for l in range(L):
                 for b in range(B[l]):
+                    j += 1
                     m.addConstr(
                         gp.quicksum(
                             x_pitbl[p,i,t,b,l] for p in range(P) for i in range(Vmax) 
                         )
                         <= 
                         v[l,b]
-                        , "C47")
+                        , "C47_" + str(j))
 
         # Add constraint: C48
+        j = 0
         for p in range(P):
             for i in range(Vmax):
                 for t in range(1,Tmax):
+                    j += 1
                     m.addConstr(
                         z_pi[p,i] >= (t * 
                         gp.quicksum(
                             x_pitbl[p,i,t,b,l] for l in range(L) for b in range(B[l])
                         ))
-                        , "C48")
+                        , "C48_" + str(j))
                         
         # Add constraint: C49
+        j = 0
         for p in range(P):
             for i in range(Vmax):
+                    j += 1
                     m.addConstr(
                         z >= z_pi[p,i]
-                        , "C49")
+                        , "C49_" + str(j))
 
 
         # Add constraint: C50
@@ -168,10 +186,11 @@ def solucionar(P, GF, MC, DS, C, B, L, gf, mc, ds, v, Cmax, Vmax, Tmax, alpha1, 
         # Optimize model
         m.update()
         m.optimize()
-        #m.computeIIS()
-        #m.write('model.ilp')
-
+        # m.computeIIS()
+        # m.write('model.ilp')
+        # print(m.display())
         m.write(arqmodel + '.sol') 
+        m.write(arqmodel + '.lp') 
 
         print('Custo: %g' % (C50.getValue()))
 
